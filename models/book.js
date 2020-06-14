e3aa10b47e0c6c054eb423e8681bf04ab5148429
Hung Le <hungle913@gmail.com>
2020-06-14 14:26:41 -0700

    added models for db

---
+++ models/book.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    _id: { 
+        type: String
+    },
+    title: { 
+        type: String 
+    },
+    authors: { 
+        type: Array 
+    },
+    description: { 
+        type: String 
+    },
+    image: { 
+        type: String 
+    },
+    link: { 
+        type: String 
+    }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
