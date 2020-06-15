1a21c5026a7a26e8a2209eccacdc4eed66d0476b
Hung Le <hungle913@gmail.com>
2020-06-15 16:45:14 -0700

    changed jumbotron for one with hamburger menu for smaller windows

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,12 +6,15 @@ import "./style.css";
 function Nav() {
     return (
 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <Link className="navbar-brand" to="/">Google Books</Link>
 
-        <nav className="navbar navbar-expand-lg text-light">
-            <div className="container">
-                <Link className="navbar-brand" to="/">Google Books</Link>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item">
                         <Link
                             to="/search"
                             className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
@@ -26,7 +29,7 @@ function Nav() {
                 </ul>
             </div>
         </nav>
-
+        
     );
 }
 
