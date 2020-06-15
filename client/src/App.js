215a90f5bd9c07e9218287696fba83c39bfae00e
Hung Le <hungle913@gmail.com>
2020-06-15 15:22:38 -0700

    added navbar to return

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,22 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
-import NoMatch from "./pages/NoMatch";
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
+
+// import NoMatch from "./pages/NoMatch";
+// import Search from "./pages/Search";
+// import Saved from "./pages/Saved";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-            <Route exact path="/" component={Search} />
+          <Nav />
+            {/* <Route exact path="/" component={Search} />
             <Route exact path="/search" component={Search} />
             <Route exact path="/saved" component={Saved} />
-            <Route exact path="/noMatch" component={NoMatch} />
+            <Route exact path="/noMatch" component={NoMatch} /> */}
         </div>
       </Router>
     );
